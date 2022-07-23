7da8545924c366158821f1d148c4eafd8ead2a91
AaritSahay <109850334+AaritSahay@users.noreply.github.com>
2022-07-23 15:32:54 +0530

    My first QuizGame

---
+++ Question.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many minutes are in half of a day?",
+    answer: "720",
+    options: [
+      "1000",
+      "720",
+      "910",
+      "860"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many angles are in a triangle?",
+    answer: "180",
+    options: [
+      "180",
+      "360",
+      "90",
+      "200"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Whats 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9?",
+    answer: "99",
+    options: [
+      "81",
+      "90",
+      "118",
+      "99"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What's the square root of 16? ",
+    answer: "4",
+    options: [
+      "5",
+      "10",
+      "4",
+      "60"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How much is 50 Dollars in Indian Rupee",
+    answer: "3993.85",
+    options: [
+      "2019.20",
+      "4156.90",
+      "5000.50",
+      "3993.85"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
